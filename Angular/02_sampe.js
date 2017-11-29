9a5fb990c94bd1ef77e47ee79a19c3644084d40f
CorentinIPI <32248017+CorentinIPI@users.noreply.github.com>
2017-11-29 11:03:39 +0100

    Commit 29/11/17

---
+++ Angular/02_sampe.js
@@ -0,0 +1,8 @@
+var Stylo = /** @class */ (function () {
+    function Stylo(couleur) {
+        this.couleur = couleur;
+    }
+    return Stylo;
+}());
+var s = new Stylo("Rouge");
+s.encre = 100;
